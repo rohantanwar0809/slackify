d5ce6185ea99e269e1580e5fb261373083a4b9df
Rohan Tanwar <rohantanwar0809@gmail.com>
2024-10-16 00:29:29 +0530

    📦 NEW: sdiebar item component

---
+++ src/app/workspace/[workspaceId]/sidebar-item.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import { LucideIcon } from "lucide-react";
+import Link from "next/link";
+import { IconType } from "react-icons/lib";
+
+const sidebarItemVariants = cva(
+  "flex items-center gap-1.5 justify-start font-normal h-7 px-[18px] overflow-hidden text-sm",
+  {
+    variants: {
+      variant: {
+        default: "text-[#f9edffcc]",
+        active: "text-[#481349] bg-white/90 hover:bg-white/90",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface SidebarItemProps {
+  label: string;
+  icon: LucideIcon | IconType;
+  id: string;
+  variant?: VariantProps<typeof sidebarItemVariants>["variant"];
+}
+
+export const SidebarItem: React.FC<SidebarItemProps> = ({
+  label,
+  icon: Icon,
+  id,
+  variant = "default",
+}) => {
+  const workspaceId = useWorkspaceId();
+  return (
+    <Button
+      asChild
+      variant="transparent"
+      size="sm"
+      className={cn(sidebarItemVariants({ variant }))}
+    >
+      <Link href={`/workspace/${workspaceId}/channel/${id}`}>
+        <Icon />
+        <span>{label}</span>
+      </Link>
+    </Button>
+  );
+};
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,8 +1,9 @@
 import { useCurrentMember } from "@/app/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/hooks/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader } from "lucide-react";
+import { AlertTriangle, Loader, MessageSquareText } from "lucide-react";
 import { WorkspaceHeader } from "./workspace-header";
+import { SidebarItem } from "./sidebar-item";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
@@ -37,6 +38,9 @@ export const WorkspaceSidebar = () => {
         workspace={workspace}
         isAdmin={currentMember.role === "admin"}
       />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
+      </div>
     </div>
   );
 };
